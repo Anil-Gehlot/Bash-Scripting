22cb89092c3ad846e4d3ee0b50ceef22b775dec7
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-07-28 21:20:28 +0530

    how to give single line and multiline comments

---
+++ 03_comments.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+#This is a single-line comment in Bash Script.
+
+echo "Enter your name: "
+read name
+
+echo
+
+echo "The current user name is $name"
+
+#This is another single line comment
+
+
+# multiline comments...
+
+<<com
+	this is 1st comments
+	this is 2nd comments
+	this is 3rd comments
+com
+
+echo "done for today"
