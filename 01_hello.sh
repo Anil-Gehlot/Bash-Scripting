da2d86d0427960a62a54143e89e6e3ac346f68ee
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-07-28 21:19:10 +0530

    hello world & some system variables

---
+++ 01_hello.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# to giv comments in our script 
+
+echo "hello world!!!!"
+
+echo Theh shell name is :   $BASH
+echo Shell version is :  $BASH_VERSION
+echo Home directory is :  $HOME
+echo Present working directory is:  $PWD
+
+
+name=Anil
+val=34
+echo My name is $name
+echo the value is $val
+
+echo "$val"
