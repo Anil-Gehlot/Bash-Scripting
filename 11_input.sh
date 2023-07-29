0deed640ec842340087d91dc6656ca9944267c25
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-07-29 08:37:33 +0530

    reading input from the user

---
+++ 11_input.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+# echo "Enter name: "
+# read name1
+
+# echo "Entered name: " $name1
+  
+# <<com  
+#     This is the first comment  
+#     This is the second comment  
+#     This is the third comment  
+# com
+  
+# echo "Hello World" 
+
+# echo -------------------------------------------------
+
+# echo "Enter name: "
+# read
+
+# echo $REPLY
+echo -------------------------------------------------
+
+read -p "username: " name
+read -sp "password : " pass
+echo
+
+echo "your username is : $name"
+echo "your password is : $pass" 
+echo -------------------------------------------------
