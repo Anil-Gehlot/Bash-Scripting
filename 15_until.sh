1117903cbfdddd7d9276a938b380de0a53406b25
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-07-30 09:27:08 +0530

    untill loop in bash script

---
+++ 15_until.sh
@@ -0,0 +1,21 @@
+# /bin/bash
+
+i=1
+
+until [ $i -gt 10 ]
+do
+    echo "$i"
+    ((i++))
+done
+
+
+max=5
+a=1
+b=0
+
+until [[ $a -gt $max || $b -gt $max ]]
+do
+    echo "a: $a & b: $b"
+    ((a++))
+    ((b++))
+done
