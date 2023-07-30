3cbc1a3e93f0c31d9ce88719c7989283837310ff
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-07-30 09:31:46 +0530

    how to declare and call a function

---
+++ 20_functions.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+
+# 1st typt to declare a function...
+greet(){
+    echo "hello Dunia!!!"
+}
+
+greet
+
+
+function greet1() {
+    echo "hello friends!!!"
+}
+
+greet1
+
+echo -------------------------------------------------------------------------
+
+
+# Passing arguments to the function....
+
+function msg(){
+    echo $1
+    echo $2
+    echo $3
+    echo $4
+    echo $5
+
+    return 5
+}
+
+msg "hello" "friendz" "kya" "hal" "chal"
+
+# jo value function ne return ki h.
+echo $?
