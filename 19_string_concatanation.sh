3e55fee6c7973213171d5bdcfe8f7ad23d8b3994
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-07-30 09:30:48 +0530

    concatinating strings together

---
+++ 19_string_concatanation.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+
+# Declaring first string 
+str1="welcome to "
+
+# Declaring second string...........
+str2="My world"
+
+
+#combining first & second string............
+str3=$str1$str2
+
+# printing new concatened string......
+# 1st way
+echo $str3
+
+#2nd way
+echo "$str1 Jurrasic Park"
+
+
