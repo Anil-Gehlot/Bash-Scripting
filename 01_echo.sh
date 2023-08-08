84ceaf3038c93398f15a4fa4bd9995db96aee218
Anil Gehlot <119098076+Anil-Gehlot@users.noreply.github.com>
2023-08-08 09:16:22 +0530

    use of echo

---
+++ 01_echo.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+ 
+# echo is used to print output at console.
+echo "hello world!!!!"
+
+# some system variables.
+echo "The shell name is :   $BASH"
+echo "Shell version is :  $BASH_VERSION"
+echo "Home directory is :  $HOME"
+echo "Present working directory is:  $PWD"
+
+
+name=Anil
+value=34
+echo "My name is $name"
+echo "The value is $value"
+
+echo "$value"
